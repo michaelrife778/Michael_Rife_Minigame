c4ec70fc9730ba9e3478c82288e381e74a3fb3e6
michaelrife778 <79294649+michaelrife778@users.noreply.github.com>
2021-02-28 23:24:32 -0500

    Add Unity Project

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    public float behindBound = -10f;
+    public float forwardBound = 150f;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z < behindBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z > forwardBound)
+        {
+            
+            Destroy(gameObject);
+        }
+       
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyShipPrefab;
+    public float spawnRangeX = 20;
+    public float spawnRangeZ = 20;
+    public float spawnRangeY = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnEnemyShip", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnEnemyShip()
+    {
+        
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), Random.Range(-spawnRangeY, spawnRangeY), spawnRangeZ);
+      
+        Instantiate(enemyShipPrefab, spawnPos, enemyShipPrefab.transform.rotation);
+    }
+
+}
